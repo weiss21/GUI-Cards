c811c60dc7508b1e36165595f900a03ed44d3e9c
evilmurries <36800405+evilmurries@users.noreply.github.com>
2019-04-07 10:15:12 -0700

    Created Phase3

---
+++ Phase3/Assig5.java
@@ -0,0 +1,5 @@
+/*Team 9 - W. Robleh, M. Mariscal, T. Doan, Y. Nikulyak, C. Piwarski
+* CST338 - Software Design
+* Assignment 5
+* Phase 3: This program adds the logic for a game of "High Card" to phase 2
+*/
